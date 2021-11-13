00796d431e61e64742ba348bc7849090f96a221a
hxbn-proto <chernik.lut@gmail.com>
2021-11-13 15:32:12 +0100

    created poc of game

---
+++ Assets/LevelController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    internal void Reset()
+    {
+        Debug.Log("Level Resetted!");
+    }
+
+    internal void Run()
+    {
+        Debug.Log("Level Runs!");
+    }
+
+    internal void Stop()
+    {
+        Debug.Log("Level Stopped!");
+    }
+}
